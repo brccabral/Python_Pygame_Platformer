2bfa6eaa8ca830696050dcb550a221b753d8e70a
Bruno Cabral <cabral.brc@gmail.com>
2021-10-15 10:29:34 -0700

    show all tiles

--- level.py
+++ level.py
@@ -1,5 +1,6 @@
 import pygame
 from tiles import Tile
+from settings import tile_size
 
 class Level:
     def __init__(self, level_data, surface) -> None:
@@ -9,8 +10,12 @@ class Level:
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
         for row_index, row in enumerate(layout):
-            print(row_index)
-            print(row)
+            for column_index, cell in enumerate(row):
+                if cell == 'X':
+                    x = column_index * tile_size
+                    y = row_index * tile_size
+                    tile = Tile((x,y), tile_size)
+                    self.tiles.add(tile)
 
     def run(self):
         self.tiles.draw(self.display_surface)
