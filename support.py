be6d6fb31da26c9729f5e04f5e136b57ce3b5b1d
Bruno Cabral <cabral.brc@gmail.com>
2021-10-15 11:33:59 -0700

    loads images from a given path

--- support.py
+++ support.py
@@ -2,13 +2,25 @@ from os import walk
 from os.path import join
 import sys
 
+import pygame
+
 def resource_path(relative):
     if hasattr(sys, "_MEIPASS"):
         return join(sys._MEIPASS, relative)
     return join(relative)
 
 def import_folder(path):
+    surface_list = []
+    
     for _,__,img_files in walk(path):
-        print(img_files)
+        for image in img_files:
+            full_path = join(path, image)
+            image_surface = pygame.image.load(resource_path(full_path)).convert_alpha()
+            surface_list.append(image_surface)
+    
+    return surface_list
 
-import_folder('assets/graphics/character/run')
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((5, 5))
+    print(import_folder('assets/graphics/character/run'))
