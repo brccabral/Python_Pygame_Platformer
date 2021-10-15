630be4782ebe4f7fd1755b32d4d9c60dee81877b
Bruno Cabral <cabral.brc@gmail.com>
2021-10-15 10:32:28 -0700

    added scroll variable

--- level.py
+++ level.py
@@ -4,8 +4,11 @@ from settings import tile_size
 
 class Level:
     def __init__(self, level_data, surface) -> None:
+
+        # level setup
         self.display_surface = surface
         self.setup_level(level_data)
+        self.world_shift = 0
     
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -18,5 +21,5 @@ class Level:
                     self.tiles.add(tile)
 
     def run(self):
-        self.tiles.update(1)
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
