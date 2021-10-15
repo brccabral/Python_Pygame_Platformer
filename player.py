7c9374ebb5fdbfc941a65d78da5b81265c9d8b6d
Bruno Cabral <cabral.brc@gmail.com>
2021-10-15 11:01:02 -0700

    player has gravity and can jump

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.constants import K_SPACE
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos) -> None:
@@ -6,8 +7,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
+
+        # player movement
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -18,7 +23,20 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = -1
         else:
             self.direction.x = 0
+        
+        if keys[K_SPACE]:
+            self.jump()
     
+    def apply_gravity(self):
+        # contantly updates direction.y to get acceleration sensation
+        self.direction.y += self.gravity
+        # here we change the player position
+        self.rect.y += self.direction.y
+    
+    def jump(self):
+        self.direction.y = self.jump_speed
+
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
+        self.apply_gravity()
