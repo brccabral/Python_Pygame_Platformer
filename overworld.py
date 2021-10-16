57e52c54265f343d8f914d9d3b89b008ab70f46f
Bruno Cabral <cabral.brc@gmail.com>
2021-10-16 14:31:50 -0700

    show level nodes on screen

--- overworld.py
+++ overworld.py
@@ -9,5 +9,21 @@ class Overworld:
         self.max_level = max_level
         self.current_level = start_level
 
+        # sprites
+        self.setup_nodes()
+    
+    def setup_nodes(self):
+        self.nodes = pygame.sprite.Group()
+        for node_data in levels.values():
+            node_sprite = Node(node_data['node_pos'])
+            self.nodes.add(node_sprite)
+
     def run(self):
-        pass
+        self.nodes.draw(self.display_surface)
+
+class Node(pygame.sprite.Sprite):
+    def __init__(self, pos) -> None:
+        super().__init__()
+        self.image = pygame.Surface((100,80))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(center = pos)
