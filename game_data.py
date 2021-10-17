124f90b27636c38fde79dd0d6ab746e8a21482d2
Bruno Cabral <cabral.brc@gmail.com>
2021-10-17 09:25:39 -0700

    add more elements to level data

--- game_data.py
+++ game_data.py
@@ -8,6 +8,9 @@ level_0_tiles = {
     'constraints': 'assets/levels/0/level_0_constraints.csv',
     'player': 'assets/levels/0/level_0_player.csv',
     'grass': 'assets/levels/0/level_0_grass.csv',
+    'node_pos': (110, 400),
+    'unlock': 1,
+    'node_graphics': 'assets/graphics/overworld/0'
 }
 
 level_0 = {'node_pos': (110, 400), 'content': 'this is level 0', 'unlock': 1}
