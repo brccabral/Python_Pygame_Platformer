28e68e9877dae0f889bbf52c01c430bb33eba194
Bruno Cabral <cabral.brc@gmail.com>
2021-10-19 14:15:34 -0700

    add overworld and level background musics

--- main.py
+++ main.py
@@ -9,18 +9,28 @@ from ui import UI
 class Game:
     def __init__(self) -> None:
         self.max_level: int = 0
-        self.overworld = Overworld(0, self.max_level, screen, self.create_level)
-        self.status = 'overworld'
         self.max_health: int = 100
         self.cur_health: int = 100
         self.coins: int = 0
 
         # user interface
         self.ui = UI(screen)
+        # audio
+        self.level_bg_music = pygame.mixer.Sound(resource_path('assets/audio/level_music.wav'))
+        self.level_bg_music.set_volume(0.1)
+        self.overworld_bg_music = pygame.mixer.Sound(resource_path('assets/audio/overworld_music.wav'))
+        self.overworld_bg_music.set_volume(0.1)
+
+        # overworld setup
+        self.overworld = Overworld(0, self.max_level, screen, self.create_level)
+        self.status = 'overworld'
+        self.overworld_bg_music.play(loops=-1)
     
     def create_level(self, current_level: int):
         self.level = Level(current_level, screen, self.create_overworld, self.change_coins, self.change_health)
         self.status = 'level'
+        self.overworld_bg_music.stop()
+        self.level_bg_music.play(loops=-1)
 
     def create_overworld(self, current_level: int, new_max_level: int):
         # check if the next level is unlocked
@@ -28,6 +38,8 @@ class Game:
             self.max_level = new_max_level
         self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
         self.status = 'overworld'
+        self.level_bg_music.stop()
+        self.overworld_bg_music.play(loops=-1)
     
     def change_coins(self, amount: int):
         self.coins += amount
