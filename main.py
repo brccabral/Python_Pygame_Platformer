1ecf8e55774a97f1dcdda86dfe0a47aadd146eb3
Bruno Cabral <cabral.brc@gmail.com>
2021-10-19 11:24:10 -0700

    show current health on top of health bar

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ class Game:
             self.overworld.run()
         else:
             self.level.run()
-            self.ui.show_health(50, 100)
-            self.ui.show_coins(4)
+            self.ui.show_health(self.cur_health, self.max_health)
+            self.ui.show_coins(self.coin)
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
--- ui.py
+++ ui.py
@@ -8,6 +8,9 @@ class UI:
 
         # health
         self.health_bar = pygame.image.load(resource_path('assets/graphics/ui/health_bar.png')).convert_alpha()
+        self.health_bar_topleft = (54, 39)
+        self.bar_max_width = 152
+        self.bar_height = 4
 
         # coins
         self.coin = pygame.image.load(resource_path('assets/graphics/ui/coin.png')).convert_alpha()
@@ -15,7 +18,13 @@ class UI:
         self.font = pygame.font.Font(resource_path('assets/graphics/ui/ARCADEPI.TTF'), 30)
     
     def show_health(self, current, full):
+        # the health bar image is the background
         self.display_surface.blit(self.health_bar, (20,10))
+        # the actual health indicator is just a red rectangle on top of the health bar
+        current_health_ratio = current/full
+        current_bar_width = self.bar_max_width * current_health_ratio
+        health_bar_rect = pygame.Rect(self.health_bar_topleft, (current_bar_width, self.bar_height))
+        pygame.draw.rect(self.display_surface, '#dc4949', health_bar_rect)
 
     def show_coins(self, amount):
         self.display_surface.blit(self.coin, self.coin_rect)
