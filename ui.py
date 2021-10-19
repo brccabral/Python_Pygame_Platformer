f2b81f69746108e570c80306ea3dc478d5bd2336
Bruno Cabral <cabral.brc@gmail.com>
2021-10-19 11:16:50 -0700

    show coin and amount

--- ui.py
+++ ui.py
@@ -11,9 +11,14 @@ class UI:
 
         # coins
         self.coin = pygame.image.load(resource_path('assets/graphics/ui/coin.png')).convert_alpha()
+        self.coin_rect = self.coin.get_rect(topleft = (50,61))
+        self.font = pygame.font.Font(resource_path('assets/graphics/ui/ARCADEPI.TTF'), 30)
     
     def show_health(self, current, full):
         self.display_surface.blit(self.health_bar, (20,10))
 
     def show_coins(self, amount):
-        pass
+        self.display_surface.blit(self.coin, self.coin_rect)
+        coin_amount_surface = self.font.render(str(amount), False, '#33323d')
+        coin_amount_rect = coin_amount_surface.get_rect(midleft = (self.coin_rect.right + 4, self.coin_rect.centery))
+        self.display_surface.blit(coin_amount_surface, coin_amount_rect)
